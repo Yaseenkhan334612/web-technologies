208e6666b9a49b7a795cc520b3974dc6dbb3f94f
Yaseenkhan006 <24pwbcs1391@uetpeshawar.edu.pk>
2026-06-19 20:03:32 +0500

    Empty repository

--- middleware/logger.js
+++
@@ -1,7 +0,0 @@
-const logger = (req, res, next) => {
-  const now = new Date().toLocaleString();
-  console.log(`[${now}] ${req.method} ${req.originalUrl}`);
-  next();
-};
-
-module.exports = logger;
